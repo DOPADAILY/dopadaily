0d3ddebd18fb8403b39fd84569a31b25b3e633d6
Ay7ot <ayomidotzee@gmail.com>
2026-01-05 20:49:41 +0100

    Fixed and removed service workers

--- next.config.ts
+++ next.config.ts
@@ -1,14 +1,7 @@
 import type { NextConfig } from "next";
-import withPWA from "next-pwa";
 
 const nextConfig: NextConfig = {
   /* config options here */
 };
 
-export default withPWA({
-  dest: "public",
-  register: true,
-  skipWaiting: true,
-  disable: process.env.NODE_ENV === "development",
-  buildExcludes: [/middleware-manifest\.json$/],
-})(nextConfig);
+export default nextConfig;
